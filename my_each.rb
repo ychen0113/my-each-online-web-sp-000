c6cb3a911e51a4b22ebe3e9a347058fd3e60291e
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-22 19:08:37 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,8 @@ def my_each(array) # put argument(s) here
   if block_given?
   i = 0 
   while i < array.length
-  yeild array[i]
+  yield array[i]
   i += 1 
   end 
   end
 end
-
-my_each(words) {puts word}
