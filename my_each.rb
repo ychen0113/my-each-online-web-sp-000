76db460969c1296f535dd6b1ebe0649e76872018
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-22 19:05:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(array) # put argument(s) here
   # code here
-  if yeild_
+  
   i = 0 
   while i < array.length
-  yeild array[i]
+  yeild array[i] if block_given?
   i += 1 
 end 
 end
